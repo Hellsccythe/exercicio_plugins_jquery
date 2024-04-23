cc1768b6cec8a35268f2c1fc3a1fa7ef9966ebe6
Bruno Eduardo <yuuidevil@gmail.com>
2024-04-22 22:15:03 -0400

    commit-inicial

--- js/form.js
+++ js/form.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 
     $('#ctel').mask('(00) 00000-0000', {
-        placeholder: '(xx) xxxxx-xxxx'
+        placeholder: '(00) 00000-0000'
+    })
+
+    $('#ccpf').mask('000.000.000-00', {
+        placeholder: '000.000.000-00'
+    })
+
+    $('#ccep').mask('00000-000', {
+        placeholder: '00000-000'
     })
 })
